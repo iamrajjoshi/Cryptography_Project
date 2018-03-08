85db96cb5a57942a50a37c6cd0dc17178b0001e8
Raj Joshi <geeky2307@gmail.com>
2018-03-08 11:05:51 -0500

    added header files

---
+++ Header.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+#include <Windows.h>
+#include <conio.h>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <Windows.h>
+#include <iostream>       
+#include <thread>     
+#include <chrono> 
+#include<stdlib.h>
+#include <fstream>
+#include <algorithm>
+#include <mmsystem.h>
--- main.cpp
+++ main.cpp
@@ -1,28 +1,11 @@
-#include <vector>
-#include <Windows.h>
-#include <conio.h>
-#include <cstdlib>
-#include <ctime>
-#include <iomanip>
-#include <iostream>
-#include <string>
-#include <time.h>
-#include <Windows.h>
-#include <iostream>       
-#include <thread>     
-#include <chrono> 
-#include<stdlib.h>
-#include <fstream>
-#include <algorithm>
-#include <mmsystem.h>
-
-using namespace std;
-
 #include "FunctionNames.h"
 #include "MatrixMultiplication.h"
 #include "Encrypt.h"
 #include "Decrypt.h"
 #include "UI.h"
+#include "Header.h"
+
+using namespace std;
 
 int main()
 {
